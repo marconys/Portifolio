e45c8447b4323f15e79c40cc6e491c3fd2f504c1
Marconys <marconysbatera@gmail.com>
2022-06-08 15:01:08 -0300

    retornando as configurações de matchMedia

--- sonicDeepWeb/js/restela.js
+++ sonicDeepWeb/js/restela.js
@@ -1,11 +1,11 @@
 // Função para detectar largura da tela e retornar evento conforme responsividade;
 
 function tela() {
-    if (window.matchMedia("(min-width:800px)").matches) {
-         // a viewport tem pelo menos 800 pixels de largura 
+    if (window.matchMedia("(min-width:745px)").matches) {
+         // a viewport tem pelo menos 745 pixels de largura 
         document.addEventListener("keydown", jump);
     } else {
-        // a viewport menos que 800 pixels de largura 
+        // a viewport menos que 745 pixels de largura 
         document.addEventListener("touchstart", jump);
     }
 }
