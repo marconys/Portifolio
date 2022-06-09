f3cd60259265ce6ccfbd7e2f8bb23845a9d2d979
Marconys <marconysbatera@gmail.com>
2022-06-09 15:38:23 -0300

    alterando proprieades do audio

--- sonicDeepWeb/js/audio.js
+++ sonicDeepWeb/js/audio.js
@@ -1,19 +1,26 @@
-let music = document.querySelector('.musica');
+let music = document.querySelector(".musica");
+
+let cont = 0;
+
 
 
 if (window.matchMedia("(min-width:745px)").matches) {
 
-    music = document.addEventListener('click', function () {
-        let audio = document.querySelector('audio');
-        audio.play();
-
-
-    })
+    music = document.addEventListener("click",musica );
 }
 else {
-    music = document.addEventListener('touchstart', function () {
-        let audio = document.querySelector('audio');
-        audio.play();
+    music = document.addEventListener("touchstart",musica);
+}
 
-    })
+function musica(){
+    
+    let audio = document.querySelector('audio');
+    if(cont == 0){
+        cont = 1;
+        audio.play();
+    }
+    else{
+        cont = 0;
+        audio.pause();
+    }
 }
